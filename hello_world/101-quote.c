02835f9dbe8e24ff378dfe75aa9f8f5065bc2efa
HecF519 <hectormtzfuentes@gmail.com>
2023-10-22 12:41:13 -0700

    Task88

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - printing without printf and puts
- * Return: always (1)
+ * main - A c program that prints a line to the standart error
+ * Return: 1 (Success)
  */
 int main(void)
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	
+	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
+}
