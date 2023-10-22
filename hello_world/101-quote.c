88d2d083a075a2bd7ffcfbcf53945efd8b8d1541
HecF519 <hectormtzfuentes@gmail.com>
2023-10-22 12:23:29 -0700

    task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#inclued <unistd.h>
+/**
+ * main - printing without printf and puts
+ * Return: always (1)
+ */
+int main(void)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
