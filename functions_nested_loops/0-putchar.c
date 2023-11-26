a9763bf2be25fe07cb3c26c955465f5cbcc8c111
HecF519 <hectormtzfuentes@gmail.com>
2023-11-26 13:54:42 -0800

    Task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * main -Entry point of my code,
  * 
  * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	return(0);
 }
